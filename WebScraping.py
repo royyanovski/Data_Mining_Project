435fd4d96a05aa5a58e4fb08128ec7c9f12ce9bd
Roy Yanovski <yanovskir@gmail.com>
2021-02-28 09:19:29 +0200

    Deleted unused comments, changed to specific execptions, added docstrings, corrected PEP8 violations.

--- WebScraping.py
+++ WebScraping.py
@@ -4,13 +4,17 @@ from bs4 import BeautifulSoup  # A package for parsing HTML code.
 import argparse
 
 
-url_address_pattern = 'https://www.ebay.com/sch/i.html?_from=R40&_nkw={}&_sacat=0&LH_TitleDesc=0&_pgn=1'  # The page i'm working on.
+url_address_pattern = 'https://www.ebay.com/sch/i.html?_from=R40&_nkw={}&_sacat=0&LH_TitleDesc=0&_pgn=1'
 
-NO_OF_PAGES_TO_EXAMINE = 4 # add 2 to the page no. you want it to get to.
+NO_OF_PAGES_TO_EXAMINE = 4  # add 2 to the page no. you want it to get to.
 SMARTPHONE_MODELS = ['iphone', 'samsung+galaxy', 'xiaomi+mi', 'huawei+p']
 
 
-def get_data(link_list):  # Extracting the data we are interested in.
+def get_data(link_list):
+    """
+    Receives a list of links to product pages in ebay and prints
+    their: title, price, sending country, shipping fee, and condition.
+    """
     for link in link_list:
         item_page = requests.get(link)
         item_soup = BeautifulSoup(item_page.content, 'html.parser')
@@ -23,31 +27,34 @@ def get_data(link_list):  # Extracting the data we are interested in.
         condition_elem = item_results.find('div', class_='u-flL condText')
 
         title_elem.find('span', class_='g-hdn').decompose()
-        if shipment_elem != None:
+        if shipment_elem is not None:
             shipment_elem = shipment_elem.contents[1]
-        if price_elem != None:
+        if price_elem is not None:
             price_elem.contents[1].decompose()
-        if location_elem != None and ',' in location_elem.text:
+        if location_elem is not None and ',' in location_elem.text:
             country_only = item_soup.new_tag('span')
             country_only.string = location_elem.text.strip().split(", ")[-1]
             location_elem = country_only
 
         chosen_elements = [title_elem, price_elem, location_elem, shipment_elem, freeshipping_elem,  condition_elem]
         for elem in chosen_elements:
-            if elem != None:
+            if elem is not None:
                 print(elem.text.strip())
         print()
 
 
 def roy_and_roi_webscraper(url, no_of_scraped_pages):
-    """Webscraping function for ebay. Running on a pre-determined number of search pages,
+    """
+    Webscraping function for ebay. Running on a pre-determined number of search pages,
     starting from the entered first search page. Output: prints title, price, supplier country,
-    and shipping cost for each item."""
+    and shipping cost for each item.
+    """
     for page_no in range(2, no_of_scraped_pages+2):
         links = []
         try:
             page = requests.get(url)  # Getting the HTML code.
-        except Exception:
+        except requests.exceptions.MissingSchema:
+            print('Invalid URL address.')
             sys.exit()
         else:
             soup = BeautifulSoup(page.content, 'html.parser')  # Creating BS object to work on with an HTML parser.
@@ -56,7 +63,7 @@ def roy_and_roi_webscraper(url, no_of_scraped_pages):
             for product_item in product_items:  # Getting the links for the product pages.
                 try:
                     link = product_item.find('a')['href']
-                except Exception:
+                except (TypeError, KeyError):
                     continue
                 else:
                     links.append(link)
@@ -65,8 +72,12 @@ def roy_and_roi_webscraper(url, no_of_scraped_pages):
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Scrapes product data from ebay')
-    parser.add_argument('search_words', type=str, help='products to search for', nargs='+')
+    """
+    Receives CLI arguments for the ebay web scraper.
+    """
+    parser = argparse.ArgumentParser(description='Scrapes product data from ebay.')
+    parser.add_argument('search_words', type=str, help="products to search for (if the search key is more than one "
+                                                       "word, use '_' between words.", nargs='+')
     parser.add_argument('-p', '--pages', type=int, help='number of pages to scrape for each search', default=1)
     args = parser.parse_args()
 
@@ -77,18 +88,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
-### Other Options:
-
-
-## Searching only for 'iPhone X' products (as an example):
-
-#def look_for_iphonex(text):
-#    """If the entered text is not None, the function returns strings that
-#    contain 'iphone x'"""
-#    if text != None:
-#        return 'iphone x' in text.lower()
-
-# iphonex_products = results.find_all('h3', string=lambda text: look_for_iphonex(text))
