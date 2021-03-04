354a757aa38058fc8d5ee6faf3c5556c59396177
Roy Yanovski <yanovskir@gmail.com>
2021-03-04 15:23:13 +0200

    Updated the README file, added a configuration file and implemented it into the code.

--- README.md
+++ README.md
@@ -3,9 +3,21 @@ This is a repo for the data mining project of the ITC data science course.
 
 ## Description
 The aim of this project is to extract data from ebay and analyze it.
+The scraper receives CLI arguments.
+
+Input: 
+1) search words (str)- enter words to search separated by a whitespace (enters the script as a list).
+If a search key is more than one word, use "_" between them.
+2) -p/--pages flag (int)- enter the number of pages you would like to search through, for each of the search words.
+Input pattern: $ python3 WebScraping.py search_key1 search_key2 ... -p page_num
+
+Output: The program prints out the following for each search word: product title, price, supplier's country,
+shipping cost, and condition.
+All prices are in Israeli Shekels (i.e. NIS or ILS).
 
 ## Installation
 This repo includes a requirements file for necessary packages.
+In addition, a configuration file includes all constants, tags, and the URL pattern. 
 
 ## Usage
 WebScraping.py - Receives a list of search words and a number of pages to search (during each search),
@@ -14,8 +26,6 @@ shipping fee, and seller country.
 
 ## Authors and Support
 Roy Yanovski - yanovskir@gmail.com
-
-Roi Weiss - roiweissr@gmail.com
 
 ## Project status
 Project still in development.
--- WebScraping.py
+++ WebScraping.py
@@ -2,12 +2,15 @@ import sys
 import requests  # A package for downloading HTML code from a website.
 from bs4 import BeautifulSoup  # A package for parsing HTML code.
 import argparse
+import json
 
+CFG_FILE = "/Users/royyanovski/data_mining_proj/config_filw.json"
+with open(CFG_FILE, 'r') as cf:
+    config = json.load(cf)
 
-url_address_pattern = 'https://www.ebay.com/sch/i.html?_from=R40&_nkw={}&_sacat=0&LH_TitleDesc=0&_pgn=1'
-
-NO_OF_PAGES_TO_EXAMINE = 4  # add 2 to the page no. you want it to get to.
-SMARTPHONE_MODELS = ['iphone', 'samsung+galaxy', 'xiaomi+mi', 'huawei+p']
+CON = config["constants"]
+TAG = config["tags"]
+URL_PATTERN = config["url_address_pattern"]
 
 
 def get_data(link_list):
@@ -19,21 +22,21 @@ def get_data(link_list):
         item_page = requests.get(link)
         item_soup = BeautifulSoup(item_page.content, 'html.parser')
         item_results = item_soup.find(id="Body")
-        title_elem = item_results.find('h1', class_='it-ttl')
-        price_elem = item_results.find('span', id='convbidPrice')
-        location_elem = item_results.find('div', class_='iti-eu-bld-gry')
-        shipment_elem = item_results.find('span', class_='sh-svc sh-nwr')
-        freeshipping_elem = item_results.find('span', class_='notranslate sh-fr-cst')
-        condition_elem = item_results.find('div', class_='u-flL condText')
+        title_elem = item_results.find(TAG["title_tag"], class_=TAG["title_class"])
+        price_elem = item_results.find(TAG["price_tag"], id=TAG["price_id"])
+        location_elem = item_results.find(TAG["location_tag"], class_=TAG["location_class"])
+        shipment_elem = item_results.find(TAG["shipment_tag"], class_=TAG["shipment_class"])
+        freeshipping_elem = item_results.find(TAG["freeship_tag"], class_=TAG["freeship_class"])
+        condition_elem = item_results.find(TAG["condition_tag"], class_=TAG["condition_class"])
 
-        title_elem.find('span', class_='g-hdn').decompose()
+        title_elem.find(TAG["title_torm_tag"], class_=TAG["title_torm_class"]).decompose()
         if shipment_elem is not None:
-            shipment_elem = shipment_elem.contents[1]
+            shipment_elem = shipment_elem.contents[CON["PRICE_ONLY"]]
         if price_elem is not None:
-            price_elem.contents[1].decompose()
+            price_elem.contents[CON["TO_DELETE"]].decompose()
         if location_elem is not None and ',' in location_elem.text:
             country_only = item_soup.new_tag('span')
-            country_only.string = location_elem.text.strip().split(", ")[-1]
+            country_only.string = location_elem.text.strip().split(", ")[CON["COUNTRY"]]
             location_elem = country_only
 
         chosen_elements = [title_elem, price_elem, location_elem, shipment_elem, freeshipping_elem,  condition_elem]
@@ -43,13 +46,13 @@ def get_data(link_list):
         print()
 
 
-def roy_and_roi_webscraper(url, no_of_scraped_pages):
+def roys_webscraper(url, no_of_scraped_pages):
     """
     Webscraping function for ebay. Running on a pre-determined number of search pages,
     starting from the entered first search page. Output: prints title, price, supplier country,
     and shipping cost for each item.
     """
-    for page_no in range(2, no_of_scraped_pages+2):
+    for page_no in range(CON["FIRST_PAGE"], no_of_scraped_pages + CON["FIRST_PAGE"]):
         links = []
         try:
             page = requests.get(url)  # Getting the HTML code.
@@ -59,16 +62,17 @@ def roy_and_roi_webscraper(url, no_of_scraped_pages):
         else:
             soup = BeautifulSoup(page.content, 'html.parser')  # Creating BS object to work on with an HTML parser.
             results = soup.find(id="mainContent")  # Extracting the results of the ebay search.
-            product_items = results.find_all('li', class_='s-item')  # Separating and using only the results themselves.
+            product_items = results.find_all(TAG["each_product_tag"], class_=TAG["each_product_class"])  # Separating
+            # and using only the results themselves.
             for product_item in product_items:  # Getting the links for the product pages.
                 try:
-                    link = product_item.find('a')['href']
+                    link = product_item.find(TAG["product_link_tag"])[TAG["product_link_class"]]  # Extract item links.
                 except (TypeError, KeyError):
                     continue
                 else:
                     links.append(link)
             get_data(links)
-            url = url[:-1] + str(page_no)
+            url = url[:CON["PAGE_NO"]] + str(page_no)
 
 
 def main():
@@ -82,8 +86,8 @@ def main():
     args = parser.parse_args()
 
     for model in args.search_words:
-        url_address = url_address_pattern.format(model)
-        roy_and_roi_webscraper(url_address, args.pages)
+        url_address = URL_PATTERN.format(model)
+        roys_webscraper(url_address, args.pages)
 
 
 if __name__ == "__main__":
